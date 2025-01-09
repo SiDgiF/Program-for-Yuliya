229ea78179921eb7946c66cc2d70a1448e391b44
Sergey <telmy@yandex.ru>
2025-01-10 00:30:24 +0300

    feat: add flag country

--- js/app.js
+++ js/app.js
@@ -245,49 +245,148 @@ tableBody.addEventListener("click", (event) => {
 });
 
 // Функция для отображения модального окна
+
+// Функция для получения URL изображения флага
+const countryCodes = {
+  Азербайджан: "az",
+  "Арабская Республика Египет": "eg",
+  Беларусь: "by",
+  Венесуэла: "ve",
+  Вьетнам: "vn",
+  Гана: "gh",
+  Зимбабве: "zw",
+  Израиль: "il",
+  Иордания: "jo",
+  "Йеменская Республика": "ye",
+  Казахстан: "kz",
+  Китай: "cn",
+  Нигерия: "ng",
+  Россия: "ru",
+  Сирия: "sy",
+  Судан: "sd",
+  Таджикистан: "tj",
+  Туркменистан: "tm",
+};
+
+function getFlagImage(country) {
+  const countryCode = countryCodes[country]; // Ищем код страны
+  if (!countryCode) {
+    console.warn(`Код для страны "${country}" не найден.`);
+    return ""; // Возвращаем пустую строку, если код не найден
+  }
+  return `https://flagcdn.com/w320/${countryCode}.png`; // URL флага
+}
+
 // Функция для отображения модального окна с полной информацией
 function showModal(studentData) {
+  const country = studentData.country;
+  const flagUrl = getFlagImage(country);
+
   modalDetails.innerHTML = `
 
   <div class="modal-details-item">
     <h3>Общие данные</h3>
-    <p><strong>ФИО:</strong> <span contenteditable="false" class="editable" data-key="name">${studentData.name}</span></p>
-    <p><strong>ФИО (англ):</strong> <span contenteditable="false" class="editable" data-key="nameEn">${studentData.nameEn}</span></p>
-    <p><strong>Пол:</strong> <span contenteditable="false" class="editable" data-key="gender">${studentData.gender}</span></p>
-    <p><strong>Год рождения:</strong> <span contenteditable="false" class="editable" data-key="birthYear">${studentData.birthYear}</span></p>
-    <p><strong>Страна:</strong> <span contenteditable="false" class="editable" data-key="country">${studentData.country}</span></p>
+    <p><strong>ФИО:</strong> <span contenteditable="false" class="editable" data-key="name">${
+      studentData.name
+    }</span></p>
+    <p><strong>ФИО (англ):</strong> <span contenteditable="false" class="editable" data-key="nameEn">${
+      studentData.nameEn
+    }</span></p>
+    <p><strong>Пол:</strong> <span contenteditable="false" class="editable" data-key="gender">${
+      studentData.gender
+    }</span></p>
+    <p><strong>Год рождения:</strong> <span contenteditable="false" class="editable" data-key="birthYear">${
+      studentData.birthYear
+    }</span></p>
+       <p><strong>Страна:</strong>
+        <span contenteditable="false" class="editable" data-key="country">${country}</span>
+        ${
+          flagUrl
+            ? `<img src="${flagUrl}" alt="Flag" class="country-flag" />`
+            : ""
+        }
+      </p>
+    </div>
   </div>
   <div class="modal-details-item">
     <h3>Образование</h3>
-    <p><strong>Группа:</strong> <span contenteditable="false" class="editable" data-key="group">${studentData.group}</span></p>
-    <p><strong>Факультет:</strong> <span contenteditable="false" class="editable" data-key="faculty">${studentData.faculty}</span></p>
-    <p><strong>Курс:</strong> <span contenteditable="false" class="editable" data-key="course">${studentData.course}</span></p>
-    <p><strong>Примечания:</strong> <span contenteditable="false" class="editable" data-key="note">${studentData.note}</span></p>
-    <p><strong>Год поступления:</strong> <span contenteditable="false" class="editable" data-key="enrollmentYear">${studentData.enrollmentYear}</span></p>
-    <p><strong>Год окончания:</strong> <span contenteditable="false" class="editable" data-key="graduationYear">${studentData.graduationYear}</span></p>
-    <p><strong>Примечания:</strong> <span contenteditable="false" class="editable" data-key="note2">${studentData.note2}</span></p>
-    <p><strong>Форма обучения:</strong> <span contenteditable="false" class="editable" data-key="educationForm">${studentData.educationForm}</span></p>
+    <p><strong>Группа:</strong> <span contenteditable="false" class="editable" data-key="group">${
+      studentData.group
+    }</span></p>
+    <p><strong>Факультет:</strong> <span contenteditable="false" class="editable" data-key="faculty">${
+      studentData.faculty
+    }</span></p>
+    <p><strong>Курс:</strong> <span contenteditable="false" class="editable" data-key="course">${
+      studentData.course
+    }</span></p>
+    <p><strong>Примечания:</strong> <span contenteditable="false" class="editable" data-key="note">${
+      studentData.note
+    }</span></p>
+    <p><strong>Год поступления:</strong> <span contenteditable="false" class="editable" data-key="enrollmentYear">${
+      studentData.enrollmentYear
+    }</span></p>
+    <p><strong>Год окончания:</strong> <span contenteditable="false" class="editable" data-key="graduationYear">${
+      studentData.graduationYear
+    }</span></p>
+    <p><strong>Примечания:</strong> <span contenteditable="false" class="editable" data-key="note2">${
+      studentData.note2
+    }</span></p>
+    <p><strong>Форма обучения:</strong> <span contenteditable="false" class="editable" data-key="educationForm">${
+      studentData.educationForm
+    }</span></p>
   </div>
   <div class="modal-details-item">
     <h3>Документы</h3>
-    <p><strong>Серия и номер паспорта:</strong> <span contenteditable="false" class="editable" data-key="passport">${studentData.passport}</span></p>
-    <p><strong>Приказ:</strong> <span contenteditable="false" class="editable" data-key="order">${studentData.order}</span></p>
-    <p><strong>Дата приказа:</strong> <span contenteditable="false" class="editable" data-key="orderDate">${studentData.orderDate}</span></p>
+    <p><strong>Серия и номер паспорта:</strong> <span contenteditable="false" class="editable" data-key="passport">${
+      studentData.passport
+    }</span></p>
+    <p><strong>Приказ:</strong> <span contenteditable="false" class="editable" data-key="order">${
+      studentData.order
+    }</span></p>
+    <p><strong>Дата приказа:</strong> <span contenteditable="false" class="editable" data-key="orderDate">${
+      studentData.orderDate
+    }</span></p>
   </div>
   <div class="modal-details-item">
     <h3>Контактные данные</h3>
-    <p><strong>Адрес:</strong> <span contenteditable="false" class="editable" data-key="homeAddress">${studentData.homeAddress}</span></p>
-    <p><strong>Общежитие/квартира:</strong> <span contenteditable="false" class="editable" data-key="dormOrApartment">${studentData.dormOrApartment}</span></p>
-    <p><strong>Телефон студента:</strong> <span contenteditable="false" class="editable" data-key="phoneStudent">${studentData.phoneStudent}</span></p>
-    <p><strong>Примечания:</strong> <span contenteditable="false" class="editable" data-key="note3">${studentData.note3}</span></p>
-    <p><strong>Куратор:</strong> <span contenteditable="false" class="editable" data-key="curator">${studentData.curator}</span></p>
-    <p><strong>Телефон куратора:</strong> <span contenteditable="false" class="editable" data-key="phoneCurator">${studentData.phoneCurator}</span></p>
+    <p><strong>Адрес:</strong> <span contenteditable="false" class="editable" data-key="homeAddress">${
+      studentData.homeAddress
+    }</span></p>
+    <p><strong>Общежитие/квартира:</strong> <span contenteditable="false" class="editable" data-key="dormOrApartment">${
+      studentData.dormOrApartment
+    }</span></p>
+    <p><strong>Телефон студента:</strong> <span contenteditable="false" class="editable" data-key="phoneStudent">${
+      studentData.phoneStudent
+    }</span></p>
+    <p><strong>Примечания:</strong> <span contenteditable="false" class="editable" data-key="note3">${
+      studentData.note3
+    }</span></p>
+    <p><strong>Куратор:</strong> <span contenteditable="false" class="editable" data-key="curator">${
+      studentData.curator
+    }</span></p>
+    <p><strong>Телефон куратора:</strong> <span contenteditable="false" class="editable" data-key="phoneCurator">${
+      studentData.phoneCurator
+    }</span></p>
   </div>
   <div class="modal-details-item">
     <h3>Разрешение на временное пребывание</h3>
-    <p><span contenteditable="false" class="editable" data-key="residencePermission">${studentData.residencePermission}</span></p>
-    <p><strong>с:</strong> <span contenteditable="false" class="editable" data-key="day1">${studentData.day1}</span> <span contenteditable="false" class="editable" data-key="month1">${studentData.month1}</span> <span contenteditable="false" class="editable" data-key="year1">${studentData.year1}</span></p>
-    <p><strong>по:</strong> <span contenteditable="false" class="editable" data-key="day2">${studentData.day2}</span> <span contenteditable="false" class="editable" data-key="month2">${studentData.month2}</span> <span contenteditable="false" class="editable" data-key="year2">${studentData.year2}</span></p>
+    <p><span contenteditable="false" class="editable" data-key="residencePermission">${
+      studentData.residencePermission
+    }</span></p>
+    <p><strong>с:</strong> <span contenteditable="false" class="editable" data-key="day1">${
+      studentData.day1
+    }</span> <span contenteditable="false" class="editable" data-key="month1">${
+    studentData.month1
+  }</span> <span contenteditable="false" class="editable" data-key="year1">${
+    studentData.year1
+  }</span></p>
+    <p><strong>по:</strong> <span contenteditable="false" class="editable" data-key="day2">${
+      studentData.day2
+    }</span> <span contenteditable="false" class="editable" data-key="month2">${
+    studentData.month2
+  }</span> <span contenteditable="false" class="editable" data-key="year2">${
+    studentData.year2
+  }</span></p>
 `;
   modal.style.display = "block"; // Показываем модальное окно
 
